70dd1d9243781eda31e4f3893780df1a449a6a73
Николай Лысенко <nikolya.lysenko@mail.ru>
2025-05-31 21:20:08 +0500

    fasasda

--- MyFirstConsoleApp/Program.cs
+++ MyFirstConsoleApp/Program.cs
@@ -6,7 +6,7 @@ namespace MyFirstConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello Worlsd");
             Console.ReadLine();
         }
     }
